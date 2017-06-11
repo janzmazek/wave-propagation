e11acab7d978cea8e83365e4f89ebe4104d94e7b
Jan <jan.zmazek@gmail.com>
2017-06-11 13:35:11 +0200

    Implemented more encapsulation in constructor.

--- source/constructor.py
+++ source/constructor.py
@@ -12,46 +12,46 @@ class Constructor(object):
     of the network and outputs the visualisation in the svg format.
     """
     def __init__(self, horizontals, verticals, length=100):
-        self.horizontals = horizontals
-        self.verticals = verticals
-        self.nodes = horizontals*verticals
-        self.adjacency = self.create_adjacency()
-        self.modified_adjacency = None
-        self.positions = self.create_positions(length)
+        self.__horizontals = horizontals
+        self.__verticals = verticals
+        self.__nodes = horizontals*verticals
+        self.__adjacency = self.__create_adjacency()
+        self.__modified_adjacency = None
+        self.__positions = self.__create_positions(length)
 
-    def create_adjacency(self):
+    def __create_adjacency(self):
         """
         This method returns initial adjacency matrix.
         """
-        adjacency = np.zeros((self.nodes, self.nodes), dtype=np.int)
+        adjacency = np.zeros((self.__nodes, self.__nodes), dtype=np.int)
         # Normal adjacency matrix for grid network
-        for i in range(self.nodes):
-            for j in range(self.nodes):
-                if (j == i+1 and j%self.verticals != 0) or \
-                   (j == i-1 and i%self.verticals != 0) or \
-                    j == i+self.verticals or \
-                    j == i-self.verticals:
+        for i in range(self.__nodes):
+            for j in range(self.__nodes):
+                if (j == i+1 and j%self.__verticals != 0) or \
+                   (j == i-1 and i%self.__verticals != 0) or \
+                    j == i+self.__verticals or \
+                    j == i-self.__verticals:
                     adjacency[i][j] = 1
         return adjacency
 
-    def create_positions(self, length):
+    def __create_positions(self, length):
         """
         This method returns initial positions matrix.
         """
-        positions = np.zeros((self.nodes, 2))
-        for i in range(self.nodes):
-            positions[i][0] = i%self.horizontals*length
-            positions[i][1] = i//self.horizontals*length
+        positions = np.zeros((self.__nodes, 2))
+        for i in range(self.__nodes):
+            positions[i][0] = i%self.__horizontals*length
+            positions[i][1] = i//self.__horizontals*length
         return positions
 
     def move_horizontal_line(self, i, length):
         """
         This method moves the horizontal line i.
         """
-        if i in range(self.horizontals):
-            for node in range(self.nodes):
-                if node//self.horizontals == i:
-                    self.positions[node][1] += length
+        if i in range(self.__horizontals):
+            for node in range(self.__nodes):
+                if node//self.__horizontals == i:
+                    self.__positions[node][1] += length
         else:
             raise ValueError("No such horizontal line.")
 
@@ -59,10 +59,10 @@ class Constructor(object):
         """
         This method moves the vertical line j.
         """
-        if j in range(self.verticals):
-            for node in range(self.nodes):
-                if node%self.horizontals == j:
-                    self.positions[node][0] += length
+        if j in range(self.__verticals):
+            for node in range(self.__nodes):
+                if node%self.__horizontals == j:
+                    self.__positions[node][0] += length
         else:
             raise ValueError("No such vertical line.")
 
@@ -71,41 +71,41 @@ class Constructor(object):
         This method deletes the street (i, j).
         """
         # TODO: don't allow border to be deleted...
-        if i in range(self.nodes) and j in range(self.nodes):
-            if self.adjacency[i][j] != 0:
-                self.adjacency[i][j] = 0
-                self.adjacency[j][i] = 0
+        if i in range(self.__nodes) and j in range(self.__nodes):
+            if self.__adjacency[i][j] != 0:
+                self.__adjacency[i][j] = 0
+                self.__adjacency[j][i] = 0
                 to_delete = []
-                if sum(self.adjacency[i]) == 2:
+                if sum(self.__adjacency[i]) == 2:
                     connections = []
-                    for k in range(self.nodes):
-                        if self.adjacency[i][k] == 1:
+                    for k in range(self.__nodes):
+                        if self.__adjacency[i][k] == 1:
                             connections.append(k)
-                    if (self.positions[i][0] == self.positions[connections[0]][0] and \
-                        self.positions[i][0] == self.positions[connections[1]][0]) or \
-                       (self.positions[i][1] == self.positions[connections[0]][1] and \
-                        self.positions[i][1] == self.positions[connections[1]][1]):
-                        self.adjacency[connections[0]][connections[1]] = 1
-                        self.adjacency[connections[1]][connections[0]] = 1
+                    if (self.__positions[i][0] == self.__positions[connections[0]][0] and \
+                        self.__positions[i][0] == self.__positions[connections[1]][0]) or \
+                       (self.__positions[i][1] == self.__positions[connections[0]][1] and \
+                        self.__positions[i][1] == self.__positions[connections[1]][1]):
+                        self.__adjacency[connections[0]][connections[1]] = 1
+                        self.__adjacency[connections[1]][connections[0]] = 1
                         to_delete.append(i)
-                if sum(self.adjacency[j]) == 2:
+                if sum(self.__adjacency[j]) == 2:
                     connections = []
-                    for k in range(self.nodes):
-                        if self.adjacency[j][k] == 1:
+                    for k in range(self.__nodes):
+                        if self.__adjacency[j][k] == 1:
                             connections.append(k)
-                    if (self.positions[j][0] == self.positions[connections[0]][0] and \
-                        self.positions[j][0] == self.positions[connections[1]][0]) or \
-                       (self.positions[j][1] == self.positions[connections[0]][1] and \
-                        self.positions[j][1] == self.positions[connections[1]][1]):
-                        self.adjacency[connections[0]][connections[1]] = 1
-                        self.adjacency[connections[1]][connections[0]] = 1
+                    if (self.__positions[j][0] == self.__positions[connections[0]][0] and \
+                        self.__positions[j][0] == self.__positions[connections[1]][0]) or \
+                       (self.__positions[j][1] == self.__positions[connections[0]][1] and \
+                        self.__positions[j][1] == self.__positions[connections[1]][1]):
+                        self.__adjacency[connections[0]][connections[1]] = 1
+                        self.__adjacency[connections[1]][connections[0]] = 1
                         to_delete.append(j)
                 if len(to_delete) != 0:
-                    self.adjacency = np.delete(self.adjacency, to_delete, axis=0)
-                    self.adjacency = np.delete(self.adjacency, to_delete, axis=1)
-                    self.positions = np.delete(self.positions, to_delete, axis=0)
+                    self.__adjacency = np.delete(self.__adjacency, to_delete, axis=0)
+                    self.__adjacency = np.delete(self.__adjacency, to_delete, axis=1)
+                    self.__positions = np.delete(self.__positions, to_delete, axis=0)
 
-                    self.nodes = int(self.nodes - len(to_delete))
+                    self.__nodes = int(self.__nodes - len(to_delete))
             else:
                 raise ValueError("Junctions are not neighbours.")
         else:
@@ -116,23 +116,23 @@ class Constructor(object):
         This method creates new adjacency matrix with dictionaries of keys
         (alpha, street width, street length, orientation) instead of 1s.
         """
-        self.modified_adjacency = self.adjacency.tolist() # To python structure
-        for i in range(self.nodes):
+        self.__modified_adjacency = self.__adjacency.tolist() # To python structure
+        for i in range(self.__nodes):
             for j in range(i):
-                if self.adjacency[i][j] == 1:
-                    if self.positions[i][1] == self.positions[j][1]:
-                        length = abs(self.positions[i][0] - self.positions[j][0])
-                        if self.positions[i][0] < self.positions[j][0]:
+                if self.__adjacency[i][j] == 1:
+                    if self.__positions[i][1] == self.__positions[j][1]:
+                        length = abs(self.__positions[i][0] - self.__positions[j][0])
+                        if self.__positions[i][0] < self.__positions[j][0]:
                             orientation = 0
-                        elif self.positions[i][0] > self.positions[j][0]:
+                        elif self.__positions[i][0] > self.__positions[j][0]:
                             orientation = 2
                         else:
                             raise ValueError("Points are at the same position.")
-                    elif self.positions[i][0] == self.positions[j][0]:
-                        length = abs(self.positions[i][1] - self.positions[j][1])
-                        if self.positions[i][1] < self.positions[j][1]:
+                    elif self.__positions[i][0] == self.__positions[j][0]:
+                        length = abs(self.__positions[i][1] - self.__positions[j][1])
+                        if self.__positions[i][1] < self.__positions[j][1]:
                             orientation = 1
-                        elif self.positions[i][1] > self.positions[j][1]:
+                        elif self.__positions[i][1] > self.__positions[j][1]:
                             orientation = 3
                         else:
                             raise ValueError("Points are at the same position.")
@@ -140,12 +140,12 @@ class Constructor(object):
                     else:
                         raise ValueError("Points are not colinear.")
 
-                    self.modified_adjacency[i][j] = {
+                    self.__modified_adjacency[i][j] = {
                         "alpha": alpha,
                         "width": width,
                         "length": length,
                         "orientation": orientation}
-                    self.modified_adjacency[j][i] = {
+                    self.__modified_adjacency[j][i] = {
                         "alpha": alpha,
                         "width": width,
                         "length": length,
@@ -156,10 +156,10 @@ class Constructor(object):
         This method changes the street width of street (i, j).
         """
         assert width>0
-        if i in range(self.nodes) and j in range(self.nodes):
-            if self.modified_adjacency[i][j] is not 0:
-                self.modified_adjacency[i][j]["width"] = width
-                self.modified_adjacency[j][i]["width"] = width
+        if i in range(self.__nodes) and j in range(self.__nodes):
+            if self.__modified_adjacency[i][j] is not 0:
+                self.__modified_adjacency[i][j]["width"] = width
+                self.__modified_adjacency[j][i]["width"] = width
             else:
                 raise ValueError("Junctions are not neighbours.")
         else:
@@ -170,21 +170,32 @@ class Constructor(object):
         This method changes the absorption coefficient of street (i, j).
         """
         assert alpha>=0 and alpha<=1
-        if i in range(self.nodes) and j in range(self.nodes):
-            if self.modified_adjacency[i][j] != 0:
-                self.modified_adjacency[i][j]["alpha"] = alpha
-                self.modified_adjacency[j][i]["alpha"] = alpha
+        if i in range(self.__nodes) and j in range(self.__nodes):
+            if self.__modified_adjacency[i][j] != 0:
+                self.__modified_adjacency[i][j]["alpha"] = alpha
+                self.__modified_adjacency[j][i]["alpha"] = alpha
             else:
                 raise ValueError("Junctions are not neighbours.")
         else:
             raise ValueError("Nodes out of range.")
 
+    def get_adjacency(self):
+        """
+        This getter method returns the normal adjacency matrix.
+        """
+        return self.__adjacency        
+
     def get_modified_adjacency(self):
         """
         This getter method returns the modified adjacency matrix.
         """
-        return self.modified_adjacency
+        return self.__modified_adjacency
 
+    def get_positions(self):
+        """
+        This getter method returns the positions matrix.
+        """
+        return self.__positions
 
     def output_network(self):
         """
@@ -193,14 +204,14 @@ class Constructor(object):
         with open("output.html", "w") as file:
             file.write("<html><head><title>Network representation</title></head><body>")
             file.write("<svg width='{0}' height='{1}'>\n".format(
-                self.positions[self.nodes-1][0], self.positions[self.nodes-1][1]))
-            for i in range(self.nodes):
+                self.__positions[self.__nodes-1][0], self.__positions[self.__nodes-1][1]))
+            for i in range(self.__nodes):
                 for j in range(i):
-                    if self.modified_adjacency[i][j] is not 0:
-                        [xi, yi] = self.positions[i]
-                        [xj, yj] = self.positions[j]
-                        alpha = self.modified_adjacency[i][j]["alpha"]
-                        width = self.modified_adjacency[i][j]["width"]/10
+                    if self.__modified_adjacency[i][j] is not 0:
+                        [xi, yi] = self.__positions[i]
+                        [xj, yj] = self.__positions[j]
+                        alpha = self.__modified_adjacency[i][j]["alpha"]
+                        width = self.__modified_adjacency[i][j]["width"]/10
                         file.write(
                             "<line x1='{0}' y1='{1}' x2='{2}' y2='{3}' \
                             style='stroke: rgb({4}, 0, {5}); stroke-width:{6}'/>\n".format(
--- source/tests/test_constructor.py
+++ source/tests/test_constructor.py
@@ -6,24 +6,28 @@ class TestConstructor(TestCase):
     def test_create_adjacency(self):
         # Test normal usage
         network = Constructor(4,4)
-        self.assertEqual(len(network.adjacency),16)
+        adjacency = network.get_adjacency()
+        self.assertEqual(len(adjacency),16)
 
     def test_create_positions(self):
         # Test normal usage
         network = Constructor(5,5)
-        self.assertEqual(len(network.positions),25)
-        self.assertEqual(len(network.positions[0]),2)
+        positions = network.get_positions()
+        self.assertEqual(len(positions),25)
+        self.assertEqual(len(positions[0]),2)
 
     def test_move_horizontal_line(self):
         # Test normal usage
         network1 = Constructor(4,4)
         network1.move_horizontal_line(2,50)
-        for i in range(2*network1.verticals, 3*network1.verticals):
-            self.assertEqual(network1.positions[i][1],250)
+        positions1 = network1.get_positions()
+        for i in range(2*4, 3*4):
+            self.assertEqual(positions1[i][1],250)
         network2 = Constructor(5,5)
         network2.move_horizontal_line(0,-10)
-        for i in range(0, network2.verticals):
-            self.assertEqual(network2.positions[i][1],-10)
+        positions2 = network2.get_positions()
+        for i in range(0, 5):
+            self.assertEqual(positions2[i][1],-10)
         # Test wrong usage
         with self.assertRaises(ValueError): # no such horizontal line
             network1.move_horizontal_line(-1,0)
@@ -34,12 +38,14 @@ class TestConstructor(TestCase):
         # Test normal usage
         network1 = Constructor(4,4)
         network1.move_vertical_line(3,30)
-        for i in range(3, network1.nodes, network1.verticals):
-            self.assertEqual(network1.positions[i][0],330)
+        positions1 = network1.get_positions()
+        for i in range(3, 16, 4):
+            self.assertEqual(positions1[i][0],330)
         network2 = Constructor(2,2)
         network2.move_vertical_line(1,100)
-        for i in range(1, network2.nodes, network2.verticals):
-            self.assertEqual(network2.positions[i][0],200)
+        positions2 = network2.get_positions()
+        for i in range(1, 4, 2):
+            self.assertEqual(positions2[i][0],200)
         # Test wrong usage
         with self.assertRaises(ValueError):
             network1.move_vertical_line(-1,0) # no such vertical line
@@ -48,69 +54,74 @@ class TestConstructor(TestCase):
 
     def test_delete_connection(self):
         # Test normal usage
-        network1 = Constructor(5,5)
-        network1.delete_connection(6,7)
-        self.assertEqual(network1.nodes,25)
-        network1.delete_connection(7,8)
-        self.assertEqual(network1.nodes,24)
-        network1.delete_connection(2,11)
-        network1.delete_connection(2,6)
-        self.assertEqual(network1.nodes,22)
-        self.assertEqual(len(network1.adjacency),22)
-        self.assertEqual(len(network1.positions),22)
-        network1.modify_adjacency()
-        network1.output_network()
+        network = Constructor(5,5)
+        network.delete_connection(6,7)
+        network.delete_connection(7,8)
+        network.delete_connection(2,11)
+        network.delete_connection(2,6)
+        adjacency = network.get_adjacency()
+        positions = network.get_positions()
+        self.assertEqual(len(adjacency),22)
+        self.assertEqual(len(positions),22)
+        network.modify_adjacency()
+        network.output_network()
 
     def test_modify_adjacency(self):
         # Test normal usage
-        network1 = Constructor(5,5)
-        network1.modify_adjacency()
-        self.assertEqual(len(network1.modified_adjacency),25)
-        for i in range(network1.nodes):
-            for j in range(network1.nodes):
-                if network1.adjacency[i][j]==0:
-                    self.assertTrue(network1.modified_adjacency[i][j]==0)
+        network = Constructor(5,5)
+        network.modify_adjacency()
+        adjacency = network.get_adjacency()
+        modified_adjacency = network.get_modified_adjacency()
+        self.assertEqual(len(modified_adjacency),25)
+        for i in range(25):
+            for j in range(25):
+                if adjacency[i][j]==0:
+                    self.assertTrue(modified_adjacency[i][j]==0)
                 else:
-                    self.assertFalse(network1.modified_adjacency[i][j]==0)
+                    self.assertFalse(modified_adjacency[i][j]==0)
 
     def test_change_width(self):
         # Test normal usage
-        network1 = Constructor(5,5)
-        network1.modify_adjacency(width=20)
-        network1.change_width(0,1,50)
-        self.assertEqual(network1.modified_adjacency[0][1]["width"],50)
-        self.assertEqual(network1.modified_adjacency[1][0]["width"],50)
-        for i in range(1, network1.nodes):
-            for j in range(1, network1.nodes):
-                if network1.adjacency[i][j]==1:
-                    self.assertEqual(network1.modified_adjacency[i][j]["width"],20)
+        network = Constructor(5,5)
+        network.modify_adjacency(width=20)
+        network.change_width(0,1,50)
+        adjacency = network.get_adjacency()
+        modified_adjacency = network.get_modified_adjacency()
+        self.assertEqual(modified_adjacency[0][1]["width"],50)
+        self.assertEqual(modified_adjacency[1][0]["width"],50)
+        for i in range(1, 25):
+            for j in range(1, 25):
+                if adjacency[i][j]==1:
+                    self.assertEqual(modified_adjacency[i][j]["width"],20)
         # Test wrong usage
         with self.assertRaises(AssertionError): # width=0
-            network1.change_width(0,1,0)
+            network.change_width(0,1,0)
         with self.assertRaises(AssertionError): # width<0
-            network1.change_width(0,1,-10)
+            network.change_width(0,1,-10)
         with self.assertRaises(ValueError): # nodes not neighbours
-            network1.change_width(0,2,50)
+            network.change_width(0,2,50)
         with self.assertRaises(ValueError): #nodes out of range
-            network1.change_width(0,25,20)
+            network.change_width(0,25,20)
 
     def test_change_alpha(self):
         # Test normal usage
-        network1 = Constructor(5,5)
-        network1.modify_adjacency(alpha=0.8)
-        network1.change_alpha(0,1,0.5)
-        self.assertEqual(network1.modified_adjacency[0][1]["alpha"],0.5)
-        self.assertEqual(network1.modified_adjacency[1][0]["alpha"],0.5)
-        for i in range(1, network1.nodes):
-            for j in range(1, network1.nodes):
-                if network1.adjacency[i][j]==1:
-                    self.assertEqual(network1.modified_adjacency[i][j]["alpha"],0.8)
+        network = Constructor(5,5)
+        network.modify_adjacency(alpha=0.8)
+        network.change_alpha(0,1,0.5)
+        adjacency = network.get_adjacency()
+        modified_adjacency = network.get_modified_adjacency()
+        self.assertEqual(modified_adjacency[0][1]["alpha"],0.5)
+        self.assertEqual(modified_adjacency[1][0]["alpha"],0.5)
+        for i in range(1, 25):
+            for j in range(1, 25):
+                if adjacency[i][j]==1:
+                    self.assertEqual(modified_adjacency[i][j]["alpha"],0.8)
         # Test wrong usage
         with self.assertRaises(AssertionError): # alpha>1
-            network1.change_alpha(0,1,2)
+            network.change_alpha(0,1,2)
         with self.assertRaises(AssertionError): # alpha<0
-            network1.change_alpha(0,1,-1)
+            network.change_alpha(0,1,-1)
         with self.assertRaises(ValueError): # nodes not neighbours
-            network1.change_alpha(0,2,0.5)
+            network.change_alpha(0,2,0.5)
         with self.assertRaises(ValueError): # nodes out of range
-            network1.change_alpha(0,25,0.5)
+            network.change_alpha(0,25,0.5)

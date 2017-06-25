0ec8bb7971b0d2400036861617a7e1066b2bbe3b
Jan <jan.zmazek@gmail.com>
2017-06-25 20:07:03 +0100

    Refactored GUI.

--- source/gui.py
+++ source/gui.py
@@ -9,7 +9,6 @@ from source.constructor import Constructor
 WIDTH = 600
 HEIGHT = 500
 
-
 class Gui(object):
     """
     This class of methods is a graphic user interface for wave propagation
@@ -43,51 +42,40 @@ class Gui(object):
         self.master.mainloop()
 
     def add_main_menu(self):
-        # create main menu
+        """
+        This method creates main menu.
+        """
 
         self.menubar = Menu(self.master)
 
         # Create file menu
         self.file_menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="File", menu=self.file_menu)
-        self.file_menu.add_command(label="Open", command=self.open_click)
-        self.file_menu.add_command(label="Save", command=self.save_click)
-        self.file_menu.add_command(label="Export image", command=self.export_click)
+        self.file_menu.add_command(label="Open", command=lambda: self.file_click("open"))
+        self.file_menu.add_command(label="Save", command=lambda: self.file_click("save"))
+        self.file_menu.add_command(label="Export image", command=lambda: self.file_click("export"))
 
         # create Window menu
         window_menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="Window", menu=window_menu)
-        window_menu.add_command(label="Small", command=lambda: self.change_window("small"))
-        window_menu.add_command(label="Medium", command=lambda: self.change_window("medium"))
-        window_menu.add_command(label="Big", command=lambda: self.change_window("big"))
+        window_menu.add_command(label="Small", command=lambda: self.window_click("small"))
+        window_menu.add_command(label="Medium", command=lambda: self.window_click("medium"))
+        window_menu.add_command(label="Big", command=lambda: self.window_click("big"))
 
         # Create Tools menu
         tools_menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="Tools", menu=tools_menu)
-        tools_menu.add_command(label="Delete", command=lambda: self.change_tool("delete"))
-        tools_menu.add_command(label="Modify network", command=lambda: self.change_tool("modify"))
-        tools_menu.add_command(label="Customise streets", command=lambda: self.change_tool("customise"))
+        tools_menu.add_command(label="Delete", command=lambda: self.tools_click("delete"))
+        tools_menu.add_command(label="Modify network", command=lambda: self.tools_click("modify"))
+        tools_menu.add_command(label="Customise streets", command=lambda: self.tools_click("customise"))
 
         # create Help menu
         help_menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=self.hello)
+        help_menu.add_command(label="About")
 
         # display the menu
         self.master.config(menu=self.menubar)
-
-    def hello(self):
-        print("Hello")
-
-    def change_window(self, window):
-        if self.canvas and self.tools_frame:
-            if window == "small":
-                self.canvas.config(width=WIDTH/1.5//1, height=HEIGHT/1.5//1)
-            elif window == "medium":
-                self.canvas.config(width=WIDTH, height=HEIGHT)
-            elif window == "big":
-                self.canvas.config(width=1.5*WIDTH//1, height=1.5*HEIGHT//1)
-
 
     def add_left_frame(self):
         """
@@ -143,6 +131,32 @@ class Gui(object):
                             command=self.draw_click)
         draw_button.grid()
 
+    def add_canvas(self):
+        """
+        This method destroys canvas if already instantiated and creates a new
+        one.
+        """
+        if self.canvas:
+            self.canvas.destroy()
+        self.canvas = Canvas(self.right_frame, width=WIDTH, height=HEIGHT, bg="AntiqueWhite1")
+        self.canvas.pack()
+
+    def add_tools(self):
+        """
+        This method destroys tools frame if already instantiated and creates a
+        new one.
+        """
+        if self.tools_frame:
+            self.tools_frame.destroy()
+        self.tools_frame = LabelFrame(self.left_frame,
+                                      text="Editing tools",
+                                      bd=2,
+                                      relief="ridge",
+                                      padx=10,
+                                      pady=10
+                                      )
+        self.tools_frame.pack(fill=BOTH, expand=1)
+
     def draw_click(self):
         """
         This method is executed when draw button is clicked.
@@ -159,73 +173,49 @@ class Gui(object):
         self.network_canvas = NetworkCanvas(self.canvas, self.constructor)
         MovingTools(self.network_canvas, self.tools_frame)
 
-    def open_click(self):
-        filename = filedialog.askopenfilename()
-        if filename is None:
-            return
-        with open(filename, "r") as file:
-            invalues = json.load(file)
-        horizontals = invalues["horizontals"]
-        verticals = invalues["verticals"]
-        modified_adjacency = invalues["modified_adjacency"]
+    def file_click(self, file_option):
+        """
+        This method is executed when one of the "file" menu options is clicked.
+        """
+        if file_option=="open":
+            filename = filedialog.askopenfilename()
+            if filename is None:
+                return
+            with open(filename, "r") as file:
+                invalues = json.load(file)
+            horizontals = invalues["horizontals"]
+            verticals = invalues["verticals"]
+            modified_adjacency = invalues["modified_adjacency"]
 
-        self.add_canvas()
-        self.add_tools()
-        self.constructor = Constructor(horizontals, verticals)
-        self.constructor.open_network(invalues)
-        self.network_canvas = NetworkCanvas(self.canvas, self.constructor)
-        if modified_adjacency is None:
-            ModifyingTools(self.network_canvas, self.tools_frame)
-        else:
-            ModelTools(self.network_canvas, self.tools_frame)
+            self.add_canvas()
+            self.add_tools()
+            self.constructor = Constructor(horizontals, verticals)
+            self.constructor.open_network(invalues)
+            self.network_canvas = NetworkCanvas(self.canvas, self.constructor)
+            if modified_adjacency is None:
+                ModifyingTools(self.network_canvas, self.tools_frame)
+            else:
+                ModelTools(self.network_canvas, self.tools_frame)
 
-    def add_canvas(self):
-        if self.canvas and self.tools_frame:
-            self.canvas.destroy()
-            self.tools_frame.destroy()
-        self.canvas = Canvas(self.right_frame, width=WIDTH, height=HEIGHT, bg="AntiqueWhite1")
-        self.canvas.pack()
-        self.tools_frame = LabelFrame(self.left_frame,
-                                      text="Editing tools",
-                                      bd=2,
-                                      relief="ridge",
-                                      padx=10,
-                                      pady=10
-                                      )
-        self.tools_frame.pack(fill=BOTH, expand=1)
-
-    def add_tools(self):
-        self.tools_frame = LabelFrame(self.left_frame,
-                                      text="Editing tools",
-                                      bd=2,
-                                      relief="ridge",
-                                      padx=10,
-                                      pady=10
-                                      )
-        self.tools_frame.pack(fill=BOTH, expand=1)
-
-
-
-    def save_click(self):
-        if self.canvas and self.tools_frame:
+        if not self.canvas or not self.tools_frame:
+            return # next options not available in starting window
+        if file_option=="save":
             filename = filedialog.asksaveasfile(mode='w', defaultextension=".json")
             if filename is None: # asksaveasfile return `None` if dialog closed with "cancel".
                 return
             self.constructor.save_network(filename.name)
-        else:
-            print("Nothing to save!")
-
-    def export_click(self):
-        if self.canvas and self.tools_frame:
+        elif file_option=="export":
             filename = filedialog.asksaveasfile(mode='w', defaultextension=".html")
             if filename is None:
                 return
             self.constructor.export_network(filename.name)
 
-    def change_tool(self, tool):
+    def tools_click(self, tool):
+        """
+        This method is executed when one of the "tools" menu options is clicked.
+        """
         if not self.canvas or not self.tools_frame or not self.constructor:
             return
-        self.tools_frame.destroy()
         self.add_tools()
 
         if tool=="delete":
@@ -240,3 +230,15 @@ class Gui(object):
             else:
                 self.network_canvas.unmodify_network()
                 ModifyingTools(self.network_canvas, self.tools_frame)
+
+    def window_click(self, window):
+        """
+        This method is executed when one of the "window" menu options is clicked.
+        """
+        if self.canvas and self.tools_frame:
+            if window == "small":
+                self.canvas.config(width=WIDTH/1.5//1, height=HEIGHT/1.5//1)
+            elif window == "medium":
+                self.canvas.config(width=WIDTH, height=HEIGHT)
+            elif window == "big":
+                self.canvas.config(width=1.5*WIDTH//1, height=1.5*HEIGHT//1)
